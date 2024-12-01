3a68c24124a161dddf1f0ea78ef87d88c11cd02b
cbrje <chri1423@gmail.com>
2024-12-01 20:42:07 +0000

    sync

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.fish
+            pkgs.just
             pkgs.deno
             pkgs.typst
             pkgs.tectonic
