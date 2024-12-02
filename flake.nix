2abde08fca2572d743f23bc7a4bb8964fcd4b067
cbrje <chri1423@gmail.com>
2024-12-02 09:39:03 +0000

    sync

--- flake.nix
+++ flake.nix
@@ -6,14 +6,13 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: 
+  outputs = { nixpkgs, flake-utils, ... }: 
     flake-utils.lib.eachDefaultSystem (system:
       let 
         pkgs = import nixpkgs {
           inherit system;
         };
         
-
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = [
